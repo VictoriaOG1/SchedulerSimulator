05360f22b831adddcae339260b3e0604d58fd8a0
SHINIK90 <73261988+SHINIK90@users.noreply.github.com>
2023-03-22 00:27:45 -0500

    java Philosophers

---
+++ Tarea2/javaPhil.java
@@ -0,0 +1,72 @@
+package Tarea2;
+
+public class javaPhil {
+
+    public class Philosopher implements Runnable {
+
+        // Member variables, methods defined earlier
+     
+         @Override
+         public void run() {
+             try {
+                 while (true) {
+                     
+                     // thinking
+                     doAction(System.nanoTime() + ": Thinking");
+                     synchronized (leftFork) {
+                         doAction(
+                           System.nanoTime() 
+                             + ": Picked up left fork");
+                         synchronized (rightFork) {
+                             // eating
+                             doAction(
+                               System.nanoTime() 
+                                 + ": Picked up right fork - eating"); 
+                             
+                             doAction(
+                               System.nanoTime() 
+                                 + ": Put down right fork");
+                         }
+                         
+                         // Back to thinking
+                         doAction(
+                           System.nanoTime() 
+                             + ": Put down left fork. Back to thinking");
+                     }
+                 }
+             } catch (InterruptedException e) {
+                 Thread.currentThread().interrupt();
+                 return;
+             }
+         }
+     }
+     
+
+    public static void main(String[] args) throws Exception {
+
+        final Philosopher[] philosophers = new Philosopher[5];
+        Object[] forks = new Object[philosophers.length];
+
+        for (int i = 0; i < forks.length; i++) {
+            forks[i] = new Object();
+        }
+
+        for (int i = 0; i < philosophers.length; i++) {
+            Object leftFork = forks[i];
+            Object rightFork = forks[(i + 1) % forks.length];
+
+            if (i == philosophers.length - 1) {
+                
+                // The last philosopher picks up the right fork first
+                philosophers[i] = new Philosopher(rightFork, leftFork); 
+            } else {
+                philosophers[i] = new Philosopher(leftFork, rightFork);
+            }
+            
+            Thread t 
+              = new Thread(philosophers[i], "Philosopher " + (i + 1));
+            t.start();
+        }
+    }
+}
+
