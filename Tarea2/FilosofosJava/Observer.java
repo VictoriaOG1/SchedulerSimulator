28e65de5b00698664b9d7a63c1a3f90593581d96
SHINIK90 <73261988+SHINIK90@users.noreply.github.com>
2023-03-23 23:51:50 -0500

    Java filosophers working

---
+++ Tarea2/FilosofosJava/Observer.java
@@ -0,0 +1,46 @@
+package FilosofosJava;
+
+import java.util.*;
+
+public class Observer{
+    private static Observer Instance = new Observer();
+    private static ArrayList<Integer> queue = new ArrayList<>();
+    static boolean[] forks = {true,true,true,true,true};
+
+    private Observer(){}
+
+    public static Observer getObserver(){
+        return Instance;
+    }
+
+    public static synchronized int requestToEat(int PhilosopherId){
+        if(forks[PhilosopherId] & forks[(PhilosopherId+1)%5]){
+            forks[PhilosopherId] = false;
+            forks[(PhilosopherId+1)%5] = false;
+            return 0;
+        }else{
+            if(!queue.contains(PhilosopherId)){
+                queue.add(PhilosopherId);
+            }
+            System.out.println("Phil: " + PhilosopherId + " has to wait to eat and it's on queue");
+            try {Observer.class.wait();}
+            catch (InterruptedException e) {e.printStackTrace();}
+
+            return 1;
+        }
+    }
+    public static synchronized void returnForks(int PhilosopherId){
+        forks[PhilosopherId] = true;
+        forks[(PhilosopherId+1)%5] = true;
+        System.out.println("Phil: " + PhilosopherId + " returned their forks");
+        for(int i = 0; i<5; i++){
+            if(forks[queue.get(i)] & forks[(queue.get(i)+1)%5]){
+                // notify phil
+                Observer.class.notifyAll();
+                // queue.remove(i);
+                break;
+            }
+        }
+        System.out.println("**Philosophers have been notified");
+    }
+}
---
+++ Tarea2/FilosofosJava/Philosopher.java
@@ -0,0 +1,50 @@
+package FilosofosJava;
+
+// import java.lang.*;
+
+public class Philosopher{
+    private int _id;
+    // private Observer observer = Observer.getObserver();
+    private boolean _eating = false;
+    private int _mealsLeft = 3;
+
+    public boolean FinishedEating(){
+        if(_mealsLeft == 0){return true;}
+        else{return false;}
+    }
+    public boolean isEating(){
+        return _eating;
+    }
+    public int getId(){
+        return _id;
+    }
+
+    public Philosopher(int id){_id = id;}
+
+    private synchronized void run(){
+        while(!FinishedEating()){
+            if(!_eating){
+                if(Observer.requestToEat(getId()) == 0){
+                    _eating = true;
+                    _mealsLeft--;
+                    System.out.println("Phil: " + _id + " started eating now");
+                    try {Thread.sleep(500);}
+                    catch (InterruptedException e) {e.printStackTrace();}
+                    Observer.returnForks(_id);
+                    System.out.println("Phil: " + _id + " finished eating, now thinking");
+                    try {Thread.sleep(500);}
+                    catch (InterruptedException e) {e.printStackTrace();}
+                    _eating = false;
+                    if(FinishedEating()){System.out.println("Phil: " + _id + " is done Eating for the day!");}
+                    else{System.out.println("Phil: " + _id + " is hungry now");}
+                }
+            }else{
+                System.out.println("Phil: " + _id + " it's still Eating");
+            }
+        }
+    }
+
+    Runnable task = () -> {
+        run();
+    };
+}
